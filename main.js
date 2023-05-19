e516bd2f71c02be63e5b6ebb811bad16e4c7b6b7
Kauan Sena <73291742+IK-R-S@users.noreply.github.com>
2023-05-19 20:12:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,60 @@
 const express = require('express')
+const Database = require('./database/database')
+
 // Declarando a variável app com o método da importação do módulo express
 const app = express()
 // Declarando a porta de conexão
 const port = 3000
+// Middleware express.json() para analisar corpos de solicitação JSON
+app.use(express.json())
 // Rota inicial da API
-app.get('/', (req, response) => {
+app.get('/', (req, res) => {
     try{
-        response.send({"express api running": "🥳️"})
+        res.send({"express api running": "🥳️"})
+        console.log('[NOVA REQUISIÇÃO]: /')
     }
-    catch(error){
-        console.log('Ops! Houve um erro no servidor:\n', error)
+    catch(err){
+        console.log('Ops! Houve um erro no servidor:\n', err)
+    }
+})
+// Rota de status da aplicação
+app.get('/status', (req, res) => {
+    try{
+        res.send({
+            "framework": "express",
+            "server": "online",
+            "status code": 200
+        })
+        console.log('[NOVA REQUISIÇÃO]: /status')
+    }
+    catch(err){
+        console.log('Ops! Houve um erro ao enviar sua solicitação:\n', err)
+    }
+
+})
+// Rota para registro de usuários
+app.post('/api/register', async (req, res) => {
+    try{
+        const data = req.body; // Armazenando o JSON do Body na variável "data"
+        const user = {
+            "name": data['name'],
+            "email": data['email'],
+            "password": data['password'],
+        }
+        //  Chamando a classe do banco de dados e executando seu método de Registro para usuários
+        const db = new Database()
+        const response = await db.registro(user)
+        res.send(response)
+        // Logs
+        console.log('[NOVA REQUISIÇÃO - USER REGISTER]: /api/register; [BODY]:')
+        console.log(data, '\n')
+    }
+    catch(err){
+        console.log('Ops! Houve um erro ao enviar sua solicitação:\n', err)
     }
 })
 
 // Subindo o servidor da API na porta 3000
 app.listen(port, () => {
-    console.log(`API Rodando na porta ${port}`)
+    console.log(`\nAPI Rodando em 'http://127.0.0.1:${port}' 🚀\n`)
 })
