870dbe2678d87c098ca8086f0cfc7cf21244851f
Kauan Sena <73291742+IK-R-S@users.noreply.github.com>
2023-05-20 21:28:49 -0300

    Delete database directory

--- database/server.js
+++
@@ -1,9 +0,0 @@
-const server = {
-    "host": "containers-us-west-168.railway.app",
-    "database": "railway",
-    "user": "postgres",
-    "password": "vai sonhando",
-    "port": "6936"
-}
-
-module.exports = server;
